9fdc89c1985f641595f5d66cd4314c17c410add9
Janvi Bajoria <janvibajo1@gmail.com>
2023-08-20 14:04:51 +0530

    feat: elaborated available tables accordion added

---
+++ src/assets/data/queries.js
@@ -0,0 +1,43 @@
+export const queryMap = [
+  {
+    tableQuery: "Customers",
+    query: "select * from CUSTOMERS",
+    tableFields: [
+      "customerID",
+      "companyName",
+      "contactName",
+      "contactTitle",
+      "address",
+      "city",
+      "region",
+      "postalCode",
+      "country",
+      "phone",
+      "fax",
+    ],
+    // data: customers,
+  },
+  {
+    tableQuery: "Categories",
+    query: "select * from CATEGORIES",
+    tableFields: ["categoryID", "categoryName", "description", "picture"],
+    // data: categories,
+  },
+  {
+    tableQuery: "Products",
+    query: "select * from PRODUCTS",
+    tableFields: [
+      "productID",
+      "productName",
+      "supplierID",
+      "categoryID",
+      "quantityPerUnit",
+      "unitPrice",
+      "unitsInStock",
+      "unitsOnOrder",
+      "reorderLevel",
+      "discontinued",
+    ],
+    // data: products,
+  },
+];
--- src/components/QueriesDrawer.js
+++ src/components/QueriesDrawer.js
@@ -9,9 +9,18 @@ import {
   DrawerHeader,
   DrawerBody,
   Stack,
+  Accordion,
+  AccordionItem,
+  AccordionIcon,
+  AccordionButton,
+  Text,
+  AccordionPanel,
+  Icon,
+  HStack,
 } from "@chakra-ui/react";
 import { BsFillArrowRightCircleFill } from "react-icons/bs";
 import { AiOutlineBars } from "react-icons/ai";
+import { queryMap } from "../assets/data/queries";
 
 const QueriesDrawer = ({ usePredefinedQuery, displayText }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -19,7 +28,6 @@ const QueriesDrawer = ({ usePredefinedQuery, displayText }) => {
 
   const Queryonclick = (value) => {
     usePredefinedQuery(value);
-    onClose();
   };
 
   return (
@@ -45,30 +53,40 @@ const QueriesDrawer = ({ usePredefinedQuery, displayText }) => {
 
           <DrawerBody>
             <Stack direction="column" spacing={4}>
-              <Button
-                leftIcon={<BsFillArrowRightCircleFill />}
-                onClick={() => {
-                  Queryonclick("select * from CUSTOMERS");
-                }}
-              >
-                Customers
-              </Button>
-              <Button
-                leftIcon={<BsFillArrowRightCircleFill />}
-                onClick={() => {
-                  Queryonclick("select * from PRODUCTS");
-                }}
-              >
-                Products
-              </Button>
-              <Button
-                leftIcon={<BsFillArrowRightCircleFill />}
-                onClick={() => {
-                  Queryonclick("select * from CATEGORIES");
-                }}
-              >
-                Categories
-              </Button>
+              <Accordion allowToggle>
+                {queryMap.map((items) => (
+                  <AccordionItem py={2} border="none">
+                    <h2>
+                      <AccordionButton
+                        onClick={() => {
+                          Queryonclick(items.query);
+                        }}
+                        bgColor={"blackAlpha.300"}
+                      >
+                        <HStack justifyContent={"space-between"} w={"100%"}>
+                          <Icon as={BsFillArrowRightCircleFill} />
+                          <Text fontWeight="bold">{items.tableQuery}</Text>
+                          <AccordionIcon />
+                        </HStack>
+                      </AccordionButton>
+                    </h2>
+                    {items.tableFields.map((tablefieldData) => (
+                      <AccordionPanel
+                        bgColor={"blackAlpha.300"}
+                        fontWeight="bold"
+                        pb={2}
+                        cursor={"pointer"}
+                        _hover={{ bg: "blackAlpha.100" }}
+                        onClick={() => {
+                          Queryonclick(items.query);
+                        }}
+                      >
+                        {tablefieldData}
+                      </AccordionPanel>
+                    ))}
+                  </AccordionItem>
+                ))}
+              </Accordion>
             </Stack>
           </DrawerBody>
         </DrawerContent>
