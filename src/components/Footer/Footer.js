34c553bd83e03f6ff53550412d2fc4d895219571
Ferdinand Ogama <ogamagabriel@gmail.com>
2023-10-18 15:49:22 +0300

    Adding Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,38 @@
+import { Box, Link, Text, HStack } from "@chakra-ui/react";
+import { BsGithub } from "react-icons/bs";
+import React from "react";
+
+function Footer() {
+    const currentYear = new Date().getFullYear();
+    return (
+        <Box
+            bgColor={"teal"}
+            width={"100%"}
+            p={4}
+            textAlign="center"
+            paddingTop={5}
+            paddingBottom={5}
+            color="#E7E6DA"
+        >
+            <Text fontSize="16" mb={2}>
+                SQL Online Editor - Copyright © {currentYear}
+            </Text>
+            <HStack justifyContent="center" spacing={4}>
+                <Link href="https://github.com/janvi01" isExternal>
+                    <HStack>
+                        <BsGithub fontSize="1.5rem" />
+                    </HStack>
+                </Link>
+                <Text>
+                    Found an issue? Report it{" "}
+                    <Link href="https://github.com/janvi01/sql-editor/issues/new/choose" isExternal>
+                        <u>here</u>
+                    </Link>
+                </Text>
+            </HStack>
+        </Box>
+    );
+}
+
+export default Footer;
+
--- src/pages/FrontLayout.js
+++ src/pages/FrontLayout.js
@@ -3,6 +3,8 @@ import Editor from "../components/Editor/Editor";
 import { Stack, VStack } from "@chakra-ui/react";
 import Navbar from "../components/Navbar/Navbar";
 import QueryHistory from "../components/Queries/QueryHistory";
+import Footer from "../components/Footer/Footer";
+
 
 const OutputDisplay = lazy(() => import("../components/Output/OutputDisplay"));
 
@@ -58,6 +60,7 @@ const FrontLayout = () => {
           setLoading={setLoading}
         />
       </Suspense>
+      <Footer />
     </VStack>
   );
 };
